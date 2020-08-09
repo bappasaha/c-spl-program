ff155a546e7cbe6f1b84a9b969545df53a87dfbd
Bappa Saha <69105298+bappasaha@users.noreply.github.com>
2020-08-10 00:39:55 +0600

    some basic loop program

---
+++ loop chapter 4/4.1 or.c
@@ -0,0 +1,12 @@
+b #include<stdio.h>
+
+int main()    //print one to ten//
+{
+    int n =1;
+    while(n<=10){
+        printf("%d\n", n);
+    }
+    n++;
+
+    return 0;
+}
---
+++ loop chapter 4/4.5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n =5;  // ৫ এর ঘড়ের  নামতা//
+    int i =1;
+
+    while(i<=10){
+
+        printf("%d x %d =%d\n", n,i, n*i);
+
+        i=i+1;
+    }
+
+    return 0;
+}
